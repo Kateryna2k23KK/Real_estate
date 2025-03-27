1cd7330b177b69e9bc41da8abdc01bc3d75c6301
Kateryna Yakymenko <katerynayakymenko@MacBook-Pro-Kateryna.local>
2025-03-27 17:45:54 +0200

    Update entire project with new structure and code

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
-# Создание экземпляра приложения Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder="flask_site/templates", static_folder="flask_site/static")
 
-# Маршрут для главной страницы
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-# Маршрут для страницы "О нас"
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 if __name__ == '__main__':
-    app.run(debug=True)
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')  # Flask будет искать файл в папке templates
-
-if __name__ == "__main__":
-    app.run(debug=True)
->>>>>>> 39f4f1a (Update Procfile to use main.py)
+    app.run(debug=True, port=5001)
