109d4cc2976f4a28c474a97e908455ac178e0853
Kateryna Yakymenko <katerynayakymenko@MacBook-Pro-Kateryna.local>
2025-03-27 14:50:44 +0200

    Resolved conflicts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, render_template
 
 # Создание экземпляра приложения Flask
@@ -15,3 +16,15 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+=======
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')  # Flask будет искать файл в папке templates
+
+if __name__ == "__main__":
+    app.run(debug=True)
+>>>>>>> 39f4f1a (Update Procfile to use main.py)
