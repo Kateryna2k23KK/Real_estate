1ad96817afa23ba12bd6d4ef6d8a9db0b355c92b
Kateryna Yakymenko <katia15katia155@gmail.com>
2025-03-27 14:20:38 +0200

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
+# Создание экземпляра приложения Flask
 app = Flask(__name__)
 
+# Маршрут для главной страницы
 @app.route('/')
-def home():
-    return "Hello, World!"
+def index():
+    return render_template('index.html')
+
+# Маршрут для страницы "О нас"
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
