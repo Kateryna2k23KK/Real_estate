d913989785ba6be4d6b3a6b4f060d8ad80b62c53
Kateryna Yakymenko <katerynayakymenko@MacBook-Pro-Kateryna.local>
2025-03-29 12:46:48 +0200

    Update entire project with new dependencies and changes

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ app = Flask(__name__, template_folder='templates')
 def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
