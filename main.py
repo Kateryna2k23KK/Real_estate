7dc372d9914bc2da4ec473cd5f19fb74543dbd58
Kateryna Yakymenko <katerynayakymenko@MacBook-Pro-Kateryna.local>
2025-03-29 13:14:22 +0200

    Update entire project with new dependencies and changes

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
-
+# Указываем Flask, где искать шаблоны и статические файлы
+app = Flask(__name__,
+            template_folder='templates',  # Папка с шаблонами
+            static_folder='static')       # Папка со статическими файлами
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Убираем app.run и даем возможность запускать приложение через gunicorn.
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Используем переменную окружения PORT
     app.run(debug=True, host="0.0.0.0", port=port)
