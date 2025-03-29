2898b82c55fabbc9668c4e1731bdb0c24f98255e
Kateryna Yakymenko <katerynayakymenko@MacBook-Pro-Kateryna.local>
2025-03-29 12:55:03 +0200

    Update entire project with new dependencies and changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
 import os
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+# Убираем app.run и даем возможность запускать приложение через gunicorn.
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))  # Используем переменную окружения PORT
+    app.run(debug=True, host="0.0.0.0", port=port)
