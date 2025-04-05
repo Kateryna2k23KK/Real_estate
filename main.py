9610c6b0e73c4d815bbbcbc5397830e4870eaf2b
Kateryna Yakymenko <katerynayakymenko@MacBook-Pro-Kateryna.local>
2025-04-05 14:09:33 +0300

    Updated project with new changes

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-import os
 from flask import Flask, render_template
 
-# Указываем Flask, где искать шаблоны и статические файлы
-app = Flask(__name__,
-            template_folder='templates',  # Папка с шаблонами
-            static_folder='static')       # Папка со статическими файлами
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Используем переменную окружения PORT
-    app.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
